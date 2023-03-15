a4a597dbe8a391d98933d180a83bbcb22f72883b
Apple <apple@Apples-MacBook-Air.local>
2023-03-15 16:24:44 +0530

    final

--- app.js
+++ app.js
@@ -1,70 +1,19 @@
-//arrow functions implicit return.
-// const sum = (a,b) => a + b;
-// const rollDie = () => (
-//     Math.floor(Math.random() * 6) + 1
-// )
+// document object model DOM is used to link Javascript to HTML/CSS file and make your website look so cool!!!
+//getelementbyId
+// YOUR CODE GOES IN HERE:
 
-// //default parameters
-// function rollDie(numsides = 6){
-//     return Math.floor(Math.random() * numsides) + 1
-// }
-
-// //default parameters
-// function greet(hello = "Hey there,", name){
-//     console.log(`${hello} ${name}!`)
-// }
-
-//spread
-
-// //spread in javascript object literals
-// const feline = {legs: 4, family: "Feline"}
-// const canine = {isFurry: true, family: "Canine"}
-// const catdog = {...feline, ...canine}  //spread function 
-// catdog = {...feline, ...canine, family: "Steele"} //changing the new object literal
+const container = document.querySelector('#container')
+const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/pokemon/'
+let number = prompt("How many pokemon do you want?")
 
 
-// //rest
-// function sum(){
-//     console.log(arguments)
-// }
-
-// function sum1(...nums){         //rest (...) used
-//     return nums.reduce((total, el) => total + el)
-// }
-
-// function medal(gold, silver, ...everyoneelse){
-//     console.log(`Gold medal goes to ${gold}`)
-//     console.log(`Silver medal goes to ${silver}`)
-//     console.log(`And thanks everyone else for participating ${everyoneelse}`)
-// }
-
-
-// //destructuring
-// const scores = [10000,2000,300,40,5]
-// const highscores = scores[0]
-// const secondhighscore = scores[1]
-
-// const [gold,silver,bronze,...everyoneElse] = scores;
-
-
-
-//destructuring objects
-const user = {
-    email: 'vrishiriyer@gmail.com',
-    password: 'vrishir',
-    firstName: 'Vrishir',
-    lastName: 'Iyer',
-    born: 2001,
-    died: false,
-    bio: 'Vrishir Iyer is a software and electronics engineer',
-    city: 'Ahmedabad',
-    state: 'Gujarat'
+for(let i = 0; i<number; i++){
+    const pokemon = document.createElement('div')
+    const label = document.createElement('span')
+    label.innerText = `#${i+1}`
+    const newImg = document.createElement("img")
+    newImg.src = `${baseURL}${i}.png`
+    pokemon.appendChild(newImg)
+    pokemon.appendChild(label)
+    container.appendChild(pokemon)
 }
-
-const {email, firstName, lastName, bio} = user;
-
-const {born: birthyear, died: deathyear} = user;    //extracting variables to different variable names  
-
-
-
-//destructuring params
