c1c98d8d148803ee49ae6c5937c33745d82f3b03
Apple <apple@Apples-MacBook-Air.local>
2023-03-15 18:55:18 +0530

    first

--- app.js
+++ app.js
@@ -1,19 +1,54 @@
-// document object model DOM is used to link Javascript to HTML/CSS file and make your website look so cool!!!
-//getelementbyId
-// YOUR CODE GOES IN HERE:
+// const btn = document.querySelector("#button")
 
-const container = document.querySelector('#container')
-const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/pokemon/'
-let number = prompt("How many pokemon do you want?")
+// btn.onclick = function click(){
+//     console.log("You clicked me!")
+//     console.log("Congrats")
+// }
+
+// btn.onmouseenter = () => {
+//     console.log("Stop that right now")
+// }
+
+// const h1 = document.querySelector("h1")
+// h1.onclick = () => {
+//     console.log("Now why would you click a heading you idiot?")
+// }
+
+// const btn1 = document.querySelector("#button2")
+// btn1.addEventListener("mouseup", () =>{
+//     alert('You clicked the 2nd version!!')
+// })
 
 
-for(let i = 0; i<number; i++){
-    const pokemon = document.createElement('div')
-    const label = document.createElement('span')
-    label.innerText = `#${i+1}`
-    const newImg = document.createElement("img")
-    newImg.src = `${baseURL}${i}.png`
-    pokemon.appendChild(newImg)
-    pokemon.appendChild(label)
-    container.appendChild(pokemon)
+//rgb exercise
+const button = document.querySelector("button")
+const h1 = document.querySelector("h1")
+
+button.addEventListener("click", () => {
+    const newColor = randomColor()
+    document.body.style.backgroundColor = newColor
+    h1.innerText = newColor
+})
+
+const randomColor = () => {
+    const r = Math.floor(Math.random() * 255)  
+    const g = Math.floor(Math.random() * 255)  
+    const b = Math.floor(Math.random() * 255)
+    return `rgb(${r},${g},${b})`
+}
+
+const btns = document.querySelectorAll("button")
+const h1s = document.querySelectorAll("h1")
+
+for (let button of btns){
+button.addEventListener("click",colorize)
+}
+for (let h1 of h1s){
+h1.addEventListener("click", colorize)
+
+}
+
+function colorize() {
+    this.style.backgroundColor = randomColor()
+    this.style.color = randomColor()
 }
