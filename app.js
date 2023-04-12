a89974226a2d19c4649a8a2394f94ecb8187aaaa
Apple <apple@Apples-MacBook-Air.local>
2023-04-12 13:22:13 +0530

    First

--- app.js
+++ app.js
@@ -1,81 +1,115 @@
-const p1button = document.querySelector("#p1button")
-const p2button = document.querySelector("#p2button")
-const p1display = document.querySelector("#p1score")
-const p2display = document.querySelector("#p2score")
-const reset = document.querySelector("#reset")
-const winningscoreselect = document.querySelector("#playto")
-const winner = document.querySelector("#winner")
+// // const req =  new XMLHttpRequest();
 
-let p1score = 0
-let p2score = 0
-let winningScore = 5
-let isgameover = false
+// // req.onload = () => {
+// //     console.log("IT LOADED!!"); 
+// //     console.log(this);
+// // }
+
+// // req.onerror = () => {
+// //     console.log("ERROR!");
+// //     console.log(this);
+// // }
+
+// // req.open("GET", "https://swapi.dev/apipeople/1");
+
+// // req.send();
 
 
-function win(x,y){
-    if(x>y){
-        return x
-    }else{
-        return y
-    }
-}
 
-p1button.addEventListener('click', () => {
-    if(!isgameover){
-            p1score+=1
-            if(p1score == winningScore){
-                isgameover = true
-                p1display.classList.add('winner')
-                p2display.classList.add('loser')
-                p1button.disabled = true
-                p2button.disabled = true
-                declarewinner()
-            }
-            p1display.textContent = p1score
-    }
+// // fetch("https://swapi.dev/api/people/1")
+// // .then((res) => {
+// //     console.log("Resolved!",res)
+// //     return res.json()
+// // })
+// // .then((data) => {
+// //     console.log("JSON Resolved!",data)
+// //     return fetch("https://swapi.dev/api/people/2")
+// // })
+// // .then((res) => {
+// //     console.log("SECOND REQUEST RESOLVED!")
+// //     return res.json()
+// // })
+// // .then((data) => {
+// //     console.log("JSON Resolved!",data)
+// // })
+// // .catch((e) => {
+// //     console.log("Error!",e)
+// // })
+
+
+// // const loadStarWarspeople =  async () => {
+// //     try{
+// //         const res = await fetch("https://swapi.dev/api/people/1");
+// //         const data = await res.json();
+// //         console.log(data);
+// //         const res2 = await fetch("https://swapi.dev/api/people/2");
+// //         const data2 = await res2.json();
+// //         console.log(data2);
+// //     } catch(e){
+// //         console.log("ERROR!", e)
+// //     }
+// // }
+
+// // loadStarWarspeople()
+
+
+// // axios.get("https://swapi.dev/api/people/1")
+// // .then((res) => {
+// //     console.log("RESPONSE!", res)
+// // })
+// // .catch((e) => {
+// //     console.log("ERROR!", e)
+// // });
+
+
+// // const loadStarWarspeople =  async (id) => {
+// //     try{
+// //         const res = await axios.get(`https://swapi.dev/api/people/${id}`);
+// //         console.log(res.data);
+// //     }catch(e){
+// //         console.log("ERROR!",e)
+// //     }
+// // };  
+
+// // loadStarWarspeople(3);
+
+// const button = document.querySelector('button')
+// const joke = document.querySelector("#jokes")
+// const getDadjoke = async () => {
+//     try{
+//         const config = { headers:  { Accept: 'application/json'}}
+//         const res = await axios.get('https://icanhazdadjoke.com/',config)
+//         return res.data.joke
+//     } catch(e){
+//         return 'No jokes available. Sorry!'
+//     }
+// }
+ 
+// const addNewJoke = async () => {
+//     const joketext = await getDadjoke()
+//     console.log(joketext)
+//     const newLI = document.createElement('LI')
+//     newLI.append(joketext)
+//     jokes.append(newLI)
+// }
+
+// button.addEventListener('click', addNewJoke)
+const form = document.querySelector("#searchForm");
+form.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const searchTerm = form.elements.query.value;
+    const config = {params: {q : searchTerm}}
+    const res =  await axios.get(`https://api.tvmaze.com/search/shows?`, config);
+    makeImages(res.data);
+    form.elements.query.value = '';
 })
 
-p2button.addEventListener('click', () => {
-    if(!isgameover){
-            p2score+=1
-            if(p2score == winningScore){
-                isgameover = true,
-                p1display.classList.add('winner')
-                p2display.classList.add('loser')
-                p1button.disabled = true
-                p2button.disabled = true
-                declarewinner()
-            }
-            p2display.textContent = p2score
-    }
-})
-
-function declarewinner(){
-    if(isgameover == true){
-        if(p1score>p2score){
-            winner.style.
-            winner.innerHTML = "Player 1 is the winner!"
-        }else{
-            winner.innerHTML = "Player 2 is the winner!"
+const makeImages = (shows) => {
+    for(let result of shows){
+        if(result.show.image){
+            const img = document.createElement('IMG');
+            img.src = result.show.image.medium;
+            document.body.append(img);
         }
     }
-}
-
-winningscoreselect.addEventListener('change', function(){
-    winningScore = parseInt(this.value)
-    resetgame()
-})
-reset.addEventListener('click',resetgame)
-
-function resetgame(){
-    isgameover = false
-    p1score = 0
-    p2score = 0
-    p1display.textContent = 0
-    p2display.textContent = 0
-    p1display.classList.remove('winner','loser')
-    p2display.classList.remove('winner','loser')
-    p1button.disabled = false
-    p2button.disabled = false
-    winner.innerHTML = "Component"
 }
